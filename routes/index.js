34f0c9d835584bf5fcab6b73081a2f6e0902f622
Przemek Augustyn <przemek@fullspectrumlaser.com>
2015-11-23 15:48:26 -0800

    working upload of single file with use of multer (express)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer');
+var uploading = multer({
+  dest: 'temporary..'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'PoC' });
 });
 
+router.post('/upload', uploading.single('displayImage'), function(req, res, next) {
+    console.log("upload");
+     res.status(204).end()
+
+});
 module.exports = router;
