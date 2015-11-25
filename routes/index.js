236ec6d399661b5b4deb361f39e3707b12a04b2a
Przemek Augustyn <przemek@fullspectrumlaser.com>
2015-11-25 13:25:45 -0800

    moving temporary files to a destination folder and watching (fs) changes so we can trigger post-processing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 var multer = require('multer');
 var uploading = multer({
   dest: 'temporary'
+});
+
+var destinationFolder = "dest";//has to be created
+
+//the destination folder that we are watching... triggers the transformation process
+fs.watch('dest', function (event, filename) {
+  console.log('event is: ' + event);
+  if (filename) {
+    console.log('filename provided: ' + filename);
+  } else {
+    console.log('filename not provided');
+  }
 });
 
 /* GET home page. */
@@ -12,9 +24,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/upload', uploading.single('file'), function(req, res,next) {
-    console.log("upload");
-    //debugger;
-      //console.log(req);
+    console.log("file upload");
+    var file = req.file;
+    console.log(file.originalname+ " " +file.size +" path:"+file.path);
+    console.log(file.filename);
+    //move to the destination folder
+    fs.rename(file.path, destinationFolder+ "\\"+file.originalname, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(file.path, function() {
+            if (err) throw err;
+        });
+    });
      res.status(204).end()
 });
 module.exports = router;
